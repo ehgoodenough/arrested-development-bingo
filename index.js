04b9b6cd565fd0e06a2a2cb34f62e26122bb7431
Andrew McPherson <andrewmcp333@gmail.com>
2015-06-11 15:39:00 -0400

    Rendered the data randomly to the page.

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-Array.prototype.random = function() {
-    return this[Math.floor(Math.random() * this.length)]
+var seed = null
+if(window.location.hash) {
+    seed = window.location.hash.substr(1)
+} else {
+    seed = Math.floor(Math.random() * 99999)
+    location.hash = seed
 }
+Math.seedrandom(seed)
 
-Array.prototype.shuffle = function() {
-    for(var i = this.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1))
-        var swap = this[i]
-        this[i] = this[j]
-        this[j] = swap
-    }
-    return this
-}
-
-var jokes = {}
-
+var Jokes = {}
 for(var id in recdev.labels) {
     var label = recdev.labels[id]
     var flows = recdev.flows[id].flows
@@ -23,15 +17,55 @@ for(var id in recdev.labels) {
             "label": label,
             "flows": flows
         }
-        //for(var episode in flows) {
-            //?!
-        //}
-        jokes[id] = joke
+        for(var episode in flows) {
+           var season = parseInt(episode.match(/S([0-9]+)/)[1])
+           if(Jokes[season] == undefined) {
+               Jokes[season] = new Array()
+           }
+           if(Jokes[season].indexOf(joke) == -1) {
+               Jokes[season].push(joke)
+           }
+        }
+    }
+}
+for(var season in Jokes) {
+    for(var i = Jokes[season].length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1))
+        var swap_variable = Jokes[season][i]
+        Jokes[season][i] = Jokes[season][j]
+        Jokes[season][j] = swap_variable
     }
 }
 
-for(var label in recdev.labels) {
-    if(label.match(/episode/)) {
-        //var season = parseInt(label.match(/S([0-9]+)/)[1])
+function getJokesFromSeason(season, amount) {
+    var jokes = []
+    season = season || 1
+    amount = amount || 25
+    for(var index = 0; index < amount; index++) {
+        if(index == Math.floor(amount / 2)) {
+            jokes.push({
+                "id": "joke-0",
+                "label": "\"This is Arrested Development\""
+            })
+        } else {
+            var joke = Jokes[season].shift()
+            Jokes[season].push(joke)
+            jokes.push(joke)
+        }
     }
+    return jokes
 }
+
+$(document).ready(function() {
+    function populateBingoBoard() {
+        var jokes = getJokesFromSeason(1, 25)
+        for(var index = 0; index < jokes.length; index++) {
+            $("#" + (index + 1)).find("a").html(jokes[index].label)
+            $("#" + (index + 1)).find("a").attr("href", "http://recurringdevelopments.com/#" + jokes[index].id)
+        }
+    }
+    $("button").click(function() {
+        populateBingoBoard()
+    })
+    populateBingoBoard()
+})
